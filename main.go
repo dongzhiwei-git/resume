3cdf4f4afa514a226a7588e49642a70c17fb8e47
dongzhiwei <2660847388@qq.com>
2026-01-11 23:37:21 +0800

    feat: embed templates for cloud deploy; add visit/generate metrics; China deploy doc; PORT env support

--- main.go
+++ main.go
@@ -1,17 +1,23 @@
 package main
 
 import (
+	"embed"
+	"html/template"
 	"os"
 
 	"github.com/dongzhiwei-git/resume/handlers"
 	"github.com/gin-gonic/gin"
 )
 
+//go:embed templates/*
+var templatesFS embed.FS
+
 func main() {
 	r := gin.Default()
 
 	r.Static("/static", "./static")
-	r.LoadHTMLGlob("templates/*")
+	tmpl := template.Must(template.ParseFS(templatesFS, "templates/*.html"))
+	r.SetHTMLTemplate(tmpl)
 
 	r.GET("/", handlers.Home)
 	r.GET("/editor", handlers.Editor)
